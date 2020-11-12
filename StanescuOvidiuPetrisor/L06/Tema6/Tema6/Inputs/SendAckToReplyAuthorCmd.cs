52fe211357ca1b8813bbae1c1166aacaa2ab1a69
Stanescu <45118227+ovidius98@users.noreply.github.com>
2020-11-12 17:25:48 +0200

    tema 6

---
+++ StanescuOvidiuPetrisor/L06/Tema6/Tema6/Inputs/SendAckToReplyAuthorCmd.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using Tema6.Outputs;
+
+namespace Tema6.Inputs
+{
+    public class SendAckToReplyAuthorCmd
+    {
+        CheckLanguageResult.CheckFailed ProblematicReply;
+        public SendAckToReplyAuthorCmd(CheckLanguageResult.CheckFailed problematic)
+        {
+            ProblematicReply = problematic;
+        }
+
+        public string Reply { get; }
+    }
+}
---
+++ StanescuOvidiuPetrisor/L06/Tema6/Tema6/Inputs/ValidateReplyCmd.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Tema6.Inputs
+{
+    public class ValidateReplyCmd
+    {
+        [Required]
+        public int AuthorId { get; }
+
+        [Required]
+        public int QuestionId { get; }
+
+        [Required]
+        [StringRange(10,500)]
+        public string Text { get; }
+        public string Reply { get; }
+        public ValidateReplyCmd(int authorid,int questionid,string text)
+        {
+            AuthorId = authorid;
+            QuestionId = questionid;
+            Text = text;
+        }
+
+
+    }
+}
