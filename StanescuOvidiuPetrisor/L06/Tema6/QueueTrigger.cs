369df49a3ffeb1c0bf5efc51e069bc85e020958d
Stanescu <45118227+ovidius98@users.noreply.github.com>
2020-11-12 17:42:22 +0200

    tema 6

---
+++ StanescuOvidiuPetrisor/L06/Tema6/QueueTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Models;
+
+namespace Tema6.Students
+{
+    public static class QueueTrigger
+    {
+        [FunctionName("QueueTrigger")]
+        [return: Table("students")]
+        public static StudentEntity Run([QueueTrigger("students-queue", Connection = "")] string myQueueItem, ILogger log)
+        {
+            log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+
+            var student = JsonConvert.DeserializeObject<StudentEntity>(myQueueItem);
+
+            return student;
+        }
+    }
+}
---
+++ StanescuOvidiuPetrisor/L06/Tema6/StudentEntity.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+
+namespace Models
+{
+    public class StudentEntity : TableEntity
+    {
+        public StudentEntity(string University, string Cnp)
+        {
+            this.PartitionKey = University;
+            this.RowKey = Cnp;
+        }
+
+        public StudentEntity() { }
+        public string FullName { get; set; }
+        public string Cnp { get; set; }
+        public string Email { get; set; }
+        public int Year { get; set; }
+        public string University { get; set; }
+    }
+}
